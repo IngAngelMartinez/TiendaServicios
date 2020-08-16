346204c78916dff5a6c5c9587df104dae47d7e89
Angel Martinez <neoxoner@live.com.mx>
2020-08-16 16:51:02 -0500

    Se agregan los endpoints del microservicio

---
+++ TiendaServicios.Api.Autor/Controllers/AutorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using TiendaServicios.Api.Autor.Aplicacion;
+using TiendaServicios.Api.Autor.Modelo;
+
+namespace TiendaServicios.Api.Autor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public AutorController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data) 
+        {
+            return await mediator.Send(data);
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<AutorDTO>>> Get() 
+        {
+            return await mediator.Send(new Consulta.ListaAutor());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AutorDTO>> Get(string id)
+        {
+            return await mediator.Send(new ConsultaFiltro.AutorUnico { Guid = id });
+        }
+
+    }
+}
